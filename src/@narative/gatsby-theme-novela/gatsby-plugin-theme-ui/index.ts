30bc0f951fe53847e4ffb925e26d309aafe120f6
woosungchoi <solut2000@gmail.com>
2020-06-18 02:11:07 +0900

    modify fonts

---
+++ src/@narative/gatsby-theme-novela/gatsby-plugin-theme-ui/index.ts
@@ -0,0 +1,35 @@
+import merge from 'lodash/merge';
+
+import colors from './colors';
+import tags from './tags';
+
+const breakpoints = [
+  ['phone_small', 320],
+  ['phone', 376],
+  ['phablet', 540],
+  ['tablet', 735],
+  ['desktop', 1070],
+  ['desktop_medium', 1280],
+  ['desktop_large', 1440]
+];
+
+const fonts = {
+  serif: "'Nanum Gothic',system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,BlinkMacSystemFont,Helvetica Neue,Arial,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;",
+  serifs: "'Nanum Gothic',system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,BlinkMacSystemFont,Helvetica Neue,Arial,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;",
+  sansSerif:
+    "'Nanum Gothic',system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,BlinkMacSystemFont,Helvetica Neue,Arial,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;",
+  monospace: `'Nanum Gothic',system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,BlinkMacSystemFont,Helvetica Neue,Arial,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;`
+};
+
+const colorModeTransition =
+  'background 0.25s var(--ease-in-out-quad), color 0.25s var(--ease-in-out-quad)';
+
+export default merge({
+  initialColorMode: 'light',
+  useCustomProperties: true,
+  colorModeTransition,
+  colors,
+  fonts,
+  breakpoints,
+  tags
+});
